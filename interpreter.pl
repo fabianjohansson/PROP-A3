c2c070be62b179c9ccfbda49a93e477d5b1157bf
MichelEll <49308309+MichelEll@users.noreply.github.com>
2020-01-09 15:46:15 +0100

    Update interpreter.pl

--- interpreter.pl
+++ interpreter.pl
@@ -29,6 +29,43 @@ parse(-ParseTree)-->
 
 /* WRITE YOUR CODE FOR THE PARSER HERE */
 	
+	
+parse(ParseTree) --> block(ParseTree).
+
+block(block(L_curly, Stmts, R_curly)) -->  left_curly(L_curly), statement(Stmts), right_curly(R_curly).
+
+statement(statement) --> [].
+statement(statement(Assign, Stmts)) --> assign(Assign), statement(Stmts).
+
+assign(assign(Id, Equal, Expr, Semicolon)) --> ident(Id), assign_op(Equal), expr(Expr), semicolon(Semicolon).
+
+expr(expr(Term)) --> term(Term).
+expr(expr(Term, Add_op, Expr) --> term(Term), add_op(Add_op), expr(Expr).
+expr(expr(Term, Sub_op, Expr) --> term(Term), sub_op(Sub_op), expr(Expr).
+
+term(term(Factor)) --> factor(Factor).
+term(term(Factor, Mult_op, Term)) --> term(Factor), mult_op(Mult_op), term(Term).
+term(term(Factor, Div_op, Term)) --> term(Factor), div_op(Div_op), term(Term).
+
+factor(factor(Int) --> int(Int).
+factor(factor(Ident) --> ident(Ident). 
+factor(factor(Left_Paren, Expr, Right_Paren) --> left_paren(Left_Paren), expr(Expr), right_paren(Right_Paren).
+
+right_curly(right_curly) --> ['}'].
+left_curly(left_curly) --> ['{'].
+ident(ident(Ident)) --> [Ident], {atom(Ident)}.
+assign_op(assign_op) --> [=].
+semicolon(semicolon) --> [;].
+add_op(add_op) --> [+]. 
+sub_op(sub_op) --> [-].
+div_op(div_op) --> [/].
+mult_op(mult_op) --> [*].
+right_paren(right_paren) -->[)].
+left_paren(left_paren) --> [(].
+int(int(Int)) --> [Int], {integer(Int)}.
+	
+	
+	
 /***
 evaluate(+ParseTree,+VariablesIn,-VariablesOut):-
 	Evaluates a parse-tree and returns the state of the program
